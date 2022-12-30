3a30aa00bcbdf617687c5ee9920c1057db0f4b5f
Andrey Soares <76456810+soarexs@users.noreply.github.com>
2022-12-30 14:26:08 -0300

    refactor: changes on code

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-const firstAlphabetCode = 65
-const lastAlphabetCode = 90
-
-const alphabetLenght = Math
-  .abs(firstAlphabetCode - lastAlphabetCode)
-
-const alphabet = Array(alphabetLenght + 1).fill(0)
-  .map((_, i) => (
-  String.fromCharCode(firstAlphabetCode + i)
-))
-
-const hashAlphabet = (alphabet) => {
-  const alphabetCode = alphabet.map((e) => (
+const hashAlphabet = (phrase) => {
+  const phraseCode = phrase.split('').map((e) => (
         e.charCodeAt(0) + 2
   ))
 
-  const hashedAlphabet = alphabetCode.map(e => (
+  const hashedphrase = phraseCode.map(e => (
     String.fromCharCode(e)
   ))
   
-  return hashedAlphabet
+  return hashedphrase.join('')
 }
 
-console.log(hashAlphabet(alphabet))
+console.log(hashAlphabet('ABC'))
